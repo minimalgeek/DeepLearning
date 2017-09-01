ce76fcd4610237b1ed728ddc07cb89b5afdbca7a
Faragó Balázs <minimalgeek@gmail.com>
2017-09-01 17:13:17 +0200

    Environment data preprocessing

--- Stock/deepstock/deepstock/environment.py
+++ Stock/deepstock/deepstock/environment.py
@@ -2,10 +2,16 @@ import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 import datetime
+import logging
+from sklearn.preprocessing import StandardScaler
 
 from .action import Action
 
+LOGGER = logging.getLogger(__name__)
+
+
 class Environment:
+    TRAIN_DATA_PCT = 0.75
 
     def __init__(self,
                  ticker_list,
@@ -35,7 +41,28 @@ class Environment:
                              for days in days_to_holds
                              for ticker in ticker_list]
 
+        self.preprocess_data()
         self.reset()
+
+    def preprocess_data(self):
+        all_data_unstacked = self.data.unstack(level=0)
+        all_data_unstacked = all_data_unstacked.pct_change().fillna(0)
+
+        rows = all_data_unstacked.shape[0]
+        train_size = int(rows * Environment.TRAIN_DATA_PCT)
+        test_size = rows - train_size
+        LOGGER.info('Data size: %d, train size: %d, test_size: %d' % (rows, train_size, test_size))
+
+        scaler = StandardScaler()
+
+        train_X = all_data_unstacked.iloc[0:train_size]
+        test_X = all_data_unstacked.iloc[train_size:]
+
+        train_X_scaled = scaler.fit_transform(train_X)
+        test_X_scaled = scaler.transform(test_X)
+
+        self.train_X_df = pd.DataFrame(data=train_X_scaled, columns=train_X.columns)
+        self.test_X_df = pd.DataFrame(data=test_X_scaled, columns=test_X.columns)
 
     def reset(self):
         self.deposit = self.initial_deposit
