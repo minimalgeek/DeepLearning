06a88e9e1cf096ff18a1b1a6d27e7a8a74eba249
Faragó Balázs <minimalgeek@gmail.com>
2017-09-01 11:53:10 +0200

    Project setup

---
+++ Stock/deepstock/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+def requirements():
+    with open('requirements.txt') as f:
+        return f.readlines()
+
+setup(name='deepstock',
+      version='0.1',
+      description='Stock trading with Deep Reinforcement Learning',
+      long_description=readme(),
+      keywords='deep learning neural network reinforcement stock trading DQN',
+      url='https://github.com/minimalgeek/DeepLearning/tree/master/Stock/deepstock',
+      author='Farago Balazs',
+      author_email='farago.balazs87@gmail.com',
+      license='MIT',
+      packages=['deepstock'],
+      install_requires=requirements(),
+      tests_require=['pytest'],
+      setup_requires=['pytest-runner'],
+      include_package_data=True,
+      zip_safe=False)
