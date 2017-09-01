3cb748c5e05320fecaae4f65c447d8532551be19
Faragó Balázs <minimalgeek@gmail.com>
2017-09-01 17:15:35 +0200

    Test

--- Stock/deepstock/tests/test_environment.py
+++ Stock/deepstock/tests/test_environment.py
@@ -13,3 +13,5 @@ def environment():
 def test_init(environment: Environment):
     # buy1...buy10,sell1...sell10,skip1...skip10
     assert len(environment.action_space) == 90
+    assert environment.train_X_df.shape == (1888, 15)
+    assert environment.test_X_df.shape == (630, 15)
