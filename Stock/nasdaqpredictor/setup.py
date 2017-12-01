656f9f3a2f534352fa4e096316211cd3e31fc8e4
minimalgeek <minimalgeek@gmail.com>
2017-12-01 11:07:07 +0100

    Setup file

---
+++ Stock/nasdaqpredictor/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Nasdaq Predictor',
+      version='1.0',
+      description='Predicts stock stuff',
+      author='Faragó Balázs',
+      author_email='minimalgeek@gmail.com',
+      packages=['nasdaqpredictor'])
